b130aeeedf07ed3f6bd76dde2ebec24b5ac1d75c
Austin Derrow-Pinion <derrowap@rose-hulman.edu>
2016-03-04 04:44:13 -0500

    commented js code

--- js/derrowap.js
+++ js/derrowap.js
@@ -1,3 +1,15 @@
+/*
+Author: 		Austin Derrow-Pinion
+Purpose: 		Develop website to showcase projects and skills
+Technologies:	HTML, JavaScript, CSS, Bootstrap, JQuery, 
+				backgroundVideo plugin, Google Doc pdf viewer plugin,
+				Github, CloudFlare, GoDaddy
+Description:
+This website was to further my experience with web development and learn
+new technologies used with web development. By showcasing my projects and
+information about me, I am able to easily provide employers with details
+of my experience and talents.
+*/
 $(document).ready(function() {
 	$('#header #myNavbar a[href="' + this.location.pathname + '"]').parent().addClass('active');
 	$("#my-video").backgroundVideo({
